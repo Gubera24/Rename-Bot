686fd6d5ef0833d5e54f307958248c52bd4b179f
Gubera24 <114172003+Gubera24@users.noreply.github.com>
2022-11-18 20:52:57 +0530

    Create txt.py

---
+++ txt.py
@@ -0,0 +1,11 @@
+ABOUT_TXT = """
+╭───────────⍟
+├🤖 𝙼𝚈 𝙽𝙰𝙼𝙴 : {}
+├👑 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁𝚂 : <a href=https://t.me/Tamilan224>Horror Movie's Tamil</a> 
+├👨‍💻 𝙿𝚁𝙾𝙶𝚁𝙰𝙼𝙴𝚁 : <a href=https://t.me/tamilanda29>T.Mugesh</a>
+├📕 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 : <a href=https://github.com/pyrogram>𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼</a>
+├✏️ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 : <a href=https://www.python.org>𝙿𝚈𝚃𝙷𝙾𝙽 3</a>
+├💾 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴 : <a href=https://cloud.mongodb.com>𝙼𝙾𝙽𝙶𝙾𝙳𝙱</a>
+├🌀 𝙼𝚈 𝚂𝙴𝚁𝚅𝙴𝚁 : <a href=https://app.koyeb.com/deploy?type=git&repository=github.com/Gubera24/Rename-Bot&branch=main&name=rename-bot>Koyeb</a>
+├📊 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚄𝚂 : v3.6.8 [ 𝙼𝙰𝙹𝙾𝚁 ]              
+╰───────────────⍟
